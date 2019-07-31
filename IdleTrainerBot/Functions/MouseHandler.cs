db6d343993d0013c8557cde42847b82bcca7f380
Dan <Danny13653@gmail.com>
2019-07-31 15:04:04 +0100

    Added Main Functions

---
+++ IdleTrainerBot/Functions/MouseHandler.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IdleTrainerBot.Functions
+{
+    class MouseHandler
+    {
+
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        public enum MouseEventFlags : uint
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010,
+            WHEEL = 0x00000800,
+            XDOWN = 0x00000080,
+            XUP = 0x00000100
+        }
+        public static void MouseLeftClick()
+        {
+            mouse_event((uint)MouseEventFlags.LEFTDOWN, 0, 0, 0, 0);
+            Thread.Sleep((new Random().Next(20, 30)));
+            mouse_event((uint)MouseEventFlags.LEFTUP, 0, 0, 0, 0);
+            Thread.Sleep(500);
+        }
+
+        public static void MouseWheelUp()
+        {
+            mouse_event((uint)MouseEventFlags.WHEEL, 0, 0, 120, 0);
+        }
+
+        public static void MouseWheelDown()
+        {
+            mouse_event((uint)MouseEventFlags.WHEEL, 0, 0, -120, 0);
+            Thread.Sleep(1000);
+        }
+
+
+        public static void MoveCursor(Point Location, bool Click = false)
+        {
+            Cursor.Position = Location;
+            if (Click)
+            {
+                MouseLeftClick();
+            }
+            Thread.Sleep(500);
+        }
+
+        public static void MoveCursorRealPos(Point Location, bool Click = false)
+        {
+            Point ProcLocation = WindowCapture.GetProcessPosition("Nox");
+            Cursor.Position = new Point(Location.X + ProcLocation.X, Location.Y + ProcLocation.Y);
+            if (Click)
+            {
+                MouseLeftClick();
+            }
+            Thread.Sleep(500);
+        }
+
+        /// <summary>
+        /// Resets Castle Screen by scrolling to the bottom
+        /// </summary>
+        public static void ResetCastle()
+        {
+
+            for (int i = 0; i < 8; i++)
+            {
+                Thread.Sleep(300);
+                MouseWheelDown();
+            }
+        }
+
+        //public static void MoveCursor(Point Location, Color ColourToCheck, Point ProcLocation)
+        //{
+        //    Bitmap BMP = WindowCapture.CaptureApplication("Nox");
+        //    if (BMP.GetPixel(Location.X, Location.Y) == ColourToCheck)
+        //    {
+        //        Cursor.Position = new Point(Location.X + ProcLocation.X, Location.Y + ProcLocation.Y);
+        //        //MouseClick();
+        //    }
+        //    else
+        //    {
+        //        Cursor.Position = new Point(Location.X + ProcLocation.X, Location.Y + ProcLocation.Y);
+
+        //        //MessageBox.Show("Invalid Position At " + Location.ToString());
+        //    }
+        //}
+    }
+}
---
+++ IdleTrainerBot/Functions/PixelChecker.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IdleTrainerBot.Functions
+{
+    class PixelChecker
+    {
+
+        /// <summary>
+        /// Checks if a Pixel value in the process is the same as paramter passed through
+        /// </summary>
+        /// <param name="LocationToCheck"> Location of the pixel to check against </param>
+        /// <param name="ColourToCheck"> Colour of the pixel to check against </param>
+        /// <param name="ClickPixel"> If true pixel will be left clicked </param>
+        /// <returns> True/False Depending on weather the pixel value matched.</returns>
+        public static Boolean CheckPixelValue(Point LocationToCheck, Color ColourToCheck, Boolean ClickPixel = false)
+        {
+            Bitmap bmp = WindowCapture.CaptureApplication("Nox");
+            Point ProcessLocation = WindowCapture.GetProcessPosition("Nox");
+            Point PixelScreenLocation = new Point(LocationToCheck.X + ProcessLocation.X, LocationToCheck.Y + ProcessLocation.Y);
+
+            if (bmp.GetPixel(LocationToCheck.X, LocationToCheck.Y) == ColourToCheck)
+            {
+                if (ClickPixel)
+                {
+                    MouseHandler.MoveCursor(PixelScreenLocation);
+                    MouseHandler.MouseLeftClick();
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public static Boolean SearchPixel(Color ColorToSearch, out Point Location)
+        {
+            //Takes Screencap of NOX
+            Bitmap ScreenCap = WindowCapture.CaptureApplication("Nox");
+
+            ScreenCap.Save("ScreenTest.bmp");
+
+            Point ProcessLocation = WindowCapture.GetProcessPosition("Nox");
+
+            Location = new Point(0, 0);
+
+            //Loops Through Each Pixel
+            for (int x = 0; x < ScreenCap.Width; x++)
+            {
+                for (int y = 0; y < ScreenCap.Height; y++)
+                {
+                    Color currentPixelColor = ScreenCap.GetPixel(x, y);
+
+                    if (ColorToSearch == currentPixelColor)
+                    {
+                        Location = new Point(x, y);
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Takes a screencap of process and gets the pixel value of location
+        /// </summary>
+        /// <param name="PixelLocation"> Pixel X & Y that you want the value of </param>
+        /// <returns> returns colour of pixel </returns>
+        public static Color GetPixelColor(Point PixelLocation)
+        {
+            Bitmap bmp = WindowCapture.CaptureApplication("Nox");
+            Color PixelValue = bmp.GetPixel(PixelLocation.X, PixelLocation.Y);
+
+            return PixelValue;
+        }
+
+    }
+}
